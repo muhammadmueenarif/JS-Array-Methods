325ce8b76be952fc27192d240e612bd2296883cd
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-05-26 17:28:12 +0500

    shift method

--- app.js
+++ app.js
@@ -4,7 +4,12 @@
 // colors.push('White');
 // console.log("Updated colors are:", colors);
 
-// Pop method used to delete element. deletes the last element and cannot delete specific index element. 
-const names= ['ali', 'ahmed', 'hassan', 'raza', 'usman'];
-names.pop();
-console.log('names are:', names);
+// // Pop method used to delete element. deletes the last element and cannot delete specific index element. 
+// const names= ['ali', 'ahmed', 'hassan', 'raza', 'usman'];
+// names.pop();
+// console.log('names are:', names);
+
+// Shift method. it is same as pop. but pop removes last element and shift removes 1 element. 
+const users =['kashan', 'ali', 'ahsan', 'ahmed', 'hassan', 'anees'];
+users.shift();
+console.log('users are:', users);
--- readme.txt
+++ readme.txt
@@ -7,3 +7,7 @@ second is pop method. it is used to remove an element from array. if we do not g
 the element at last index of array. 
 it deletes the last element and cannot delete specific index element. 
 
+third is shift method. it is same as pop. but pop removes last element and shift removes first element and then
+all other elements are shifted one place behind. 
+we can delete specific element by splice or filter method. 
+
