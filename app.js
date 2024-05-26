a2dae9784a010b5bf9dec5cef2b052a8f0ad7138
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-05-26 17:41:49 +0500

    unshift method

--- app.js
+++ app.js
@@ -4,12 +4,22 @@
 // colors.push('White');
 // console.log("Updated colors are:", colors);
 
+
 // // Pop method used to delete element. deletes the last element and cannot delete specific index element. 
 // const names= ['ali', 'ahmed', 'hassan', 'raza', 'usman'];
 // names.pop();
 // console.log('names are:', names);
 
+
 // Shift method. it is same as pop. but pop removes last element and shift removes 1 element. 
-const users =['kashan', 'ali', 'ahsan', 'ahmed', 'hassan', 'anees'];
-users.shift();
-console.log('users are:', users);
+// // it can give us the deleted element back as in below example. 
+// const users =['kashan', 'ali', 'ahsan', 'ahmed', 'hassan', 'anees'];
+// var firstuser=  users.shift();
+// console.log('users are:', users);
+// console.log('the deleted user is:', firstuser);
+
+
+// unshift method. it gives us to option to put item in array. it inserts new element at the start of array. 
+const vehicles =['cars', 'buses', 'bikes', 'cycles', 'truck'];
+vehicles.unshift('Shehzore');
+console.log("The vehicles are:", vehicles);
--- readme.txt
+++ readme.txt
@@ -8,6 +8,7 @@ the element at last index of array.
 it deletes the last element and cannot delete specific index element. 
 
 third is shift method. it is same as pop. but pop removes last element and shift removes first element and then
-all other elements are shifted one place behind. 
+all other elements are shifted one place behind. it can give us the deleted element. check example in code.
 we can delete specific element by splice or filter method. 
 
+next is unshift method. it gives us to option to put item in array. it inserts new element at the start of array. 
